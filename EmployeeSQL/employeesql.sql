fe1d9b0cf4f60e82b4656e0ac5f35b7eb60e6e26
bjouellette <67930251+bjouellette@users.noreply.github.com>
2020-09-15 21:22:14 -0500

    final .sql files

--- EmployeeSQL/employeesql.sql
+++ EmployeeSQL/employeesql.sql
@@ -48,4 +48,45 @@
 -- JOIN salaries s
 -- ON (e.emp_no = s.emp_no)
 
+-- SELECT first_name, last_name, hire_date 
+-- FROM employees
+-- WHERE hire_date between '1986-01-01' and '1986-12-31'
 
+-- SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
+-- FROM dept_manager dm
+-- INNER JOIN departments d
+-- ON (dm.dept_no = d.dept_no)
+-- INNER JOIN employees e
+-- ON (dm.emp_no = e.emp_no)
+
+-- SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+-- FROM employees e
+-- INNER JOIN dept_emp de
+-- ON (e.emp_no = de.emp_no)
+-- INNER JOIN departments d
+-- ON (de.dept_no = d.dept_no)
+
+-- SELECT first_name, last_name, sex 
+-- FROM employees
+-- WHERE first_name = 'Hercules' AND last_name LIKE 'B%'
+
+-- SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+-- FROM employees e
+-- INNER JOIN dept_emp de
+-- ON (e.emp_no = de.emp_no)
+-- INNER JOIN departments d
+-- ON (de.dept_no = d.dept_no)
+-- WHERE d.dept_name = 'Sales'
+
+-- SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+-- FROM employees e
+-- INNER JOIN dept_emp de
+-- ON (e.emp_no = de.emp_no)
+-- INNER JOIN departments d
+-- ON (de.dept_no = d.dept_no)
+-- WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development'
+
+-- SELECT last_name, COUNT(last_name)
+-- FROM employees 
+-- GROUP BY last_name 
+-- ORDER BY COUNT(last_name) desc
---
+++ EmployeeSQL/tableschemata.sql
@@ -0,0 +1,56 @@
+-- Create a new table
+CREATE TABLE departments (
+  dept_no VARCHAR NOT NULL,
+  dept_name VARCHAR NOT NULL
+  PRIMARY KEY (dept_no)
+);
+
+-- Create a new table
+CREATE TABLE dept_emp (
+	emp_no INT,
+	dept_no VARCHAR NOT NULL
+	PRIMARY KEY (emp_no,dept_no)
+	FOREIGN KEY (emp_no) references employees.emp_no
+	FOREIGN KEY (dept_no) references departments.dept_no	
+);
+
+
+-- Create a new table
+CREATE TABLE dept_manager (
+	dept_no VARCHAR NOT NULL,
+	emp_no INT
+	PRIMARY KEY (dept_no, emp_no)
+	FOREIGN KEY (emp_no) references employees.emp_no
+	FOREIGN KEY (dept_no) references departments.dept_no
+);
+
+
+-- Create a new table
+CREATE TABLE employees (
+ emp_no INT,
+	emp_title_id VARCHAR NOT NULL,
+	birth_date DATE,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	sex VARCHAR NOT NULL,
+	hire_date DATE
+	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_title_id) references titles.title_id
+);
+
+
+-- Create a new table
+CREATE TABLE salaries (
+	emp_no INT,
+	salary INT
+	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) references employees.emp_no
+);
+
+
+-- Create a new table
+CREATE TABLE titles (
+	title_id VARCHAR NOT NULL,
+	title VARCHAR NOT NULL
+	PRIMARY KEY (title_id)
+);
